dcc0247d8209436c825390d28d01272d5232ffa9
byrnecthirty <byrnec30@tcd.ie>
2019-03-11 23:16:36 +0000

    this

--- firstscrape.py
+++ firstscrape.py
@@ -4,7 +4,6 @@ from requests.exceptions import RequestException
 from contextlib import closing
 import sqlalchemy as sqla
 from sqlalchemy import create_engine
-from bs4 import BeautifulSoup
 import json
 from pprint import pprint
 import csv
@@ -15,11 +14,9 @@ import traceback
 import glob
 import os
 from pprint import pprint
-# import simplejson as json
-import requests
+import datetime
 import time
 import mysql.connector
-from IPython.display import display
 
 headers = {
     "Accept": "application/json"
@@ -29,38 +26,68 @@ APIKEY = "24ac0c9369d539a9c2cdb7aa8bac2bd4dfaf022c"
 NAME = "Dublin"
 STATIONS_URI = "https://api.jcdecaux.com/vls/v1/stations"
 
-URI = "mydublinbikes.cts6ewdpn6n.eu-west-1.rds.amazonaws.com"
+URI = "dublinbikes.cts6ewdbpn6n.eu-west-1.rds.amazonaws.com"
 PORT = "3306"
-DB = "mydublinbikes"
-USER = "mydublinbikes"
+DB = "dublinbikes"
+USER = "dublinbikes"
 PASSWORD = "PuRple55"
 
-r = requests.get(STATIONS_URI, params={"apiKey": APIKEY, "contract": NAME}, headers = headers)
-print(r.url)
-print(r.status_code)
-# pprint(json.loads(r.text))
+# r = requests.get(STATIONS_URI, params={"apiKey": APIKEY, "contract": NAME}, headers = headers)
+# print(r.url)
+# print(r.status_code)
+# # pprint(json.loads(r.text))
 
 
-a = requests.get("{}/{}".format(STATIONS_URI, 42),
-params={"apiKey": APIKEY, "contract": NAME})
-pprint(json.loads(a.text))
-print("traceback")
+# a = requests.get("{}/{}".format(STATIONS_URI, 42),
+# params={"apiKey": APIKEY, "contract": NAME})
+# pprint(json.loads(a.text))
 
-stream_df = pd.DataFrame(columns=( 'address' , 'available_bike_stands' , 'available_bikes' , 'banking' , 'bike_stands' , 'bonus', 'contract_name' , 'last_update' , 'name' , 'number' , 'position' , 'status' ))
 
 engine = create_engine("mysql+mysqlconnector://{}:{}@{}:{}/{}".format(USER,PASSWORD,URI,PORT,DB), echo=True)
+connection = engine.connect()
+
+
+
+# stream_df = pd.DataFrame(columns=( 'address' , 'available_bike_stands' , 'available_bikes' , 'banking' , 'bike_stands' , 'bonus', 'contract_name' , 'last_update' , 'name' , 'number' , 'position' , 'status' ))
 
 
 # stream_df.to_sql(name='streams', con=engine, if_exists = 'append', index=False)
 
-sql = """
-CREATE DATABASE IF NOT EXISTS dbbikes;
-"""
-engine.execute(sql)
+# sql = "Select * from firsttable"
+
+sqldatabase = "CREATE DATABASE IF NOT EXISTS dublinbikes"
+
+connection.execute(sqldatabase)
+
+sqltable = """ 
+CREATE TABLE IF NOT EXISTS station ( 
+address VARCHAR(256),
+available_bike_stands INTEGER,
+available_bikes INTEGER,
+banking INTEGER,
+bike_stands INTEGER,
+bonus INTEGER,
+contract_name VARCHAR(256),
+last_update INTEGER, 
+name VARCHAR(256), 
+number INTEGER, 
+position_lat REAL, 
+position_lng REAL, 
+status VARCHAR(256))"""
+
+# connection.execute("INSERT into firsttable... .. . . .. . . ")
+# stream_df.to_sql(name='streams', con=engine, if_exists = 'append', index=False)
+
+try: 
+    # res = connection.execute("DROP TABLE IF EXISTS station")
+    res = connection.execute(sqltable)
+    print(res.fetchall())
+except Exception as e:
+    print(e)
+
 print("i made it")
 
 # connection = engine.connect()
-# # stream_df.to_sql(name='streams', con=engine, if_exists = 'append', index=False)
 # result = connection.execute("select username from users")
 # for row in r:
 #     print("name:", row['name'])
@@ -69,9 +96,60 @@ print("i made it")
 
 # data = r.json() 
 
-
+# this works, writes to csvfile
 # with open("guns.csv", "wb") as csvfile:
 #     f = csv.writer(csvfile)
 #     f.writerow(["address"]) # write the headers if you like
 #     for elem in data:
 #         f.writerow([])
+
+
+
+# def write_to_file(text):
+#     with open("data/bikes_{}".format(now.replace(" ", "_"), "w") as f:
+#         f.write(r.text)
+
+def stations_to_db(text):
+    stations = json.loads(text)
+    print("i am looking at the stations")
+    for station in stations:
+        print(type(station), len(station))
+        vals=(station.get('address'), station.get('available_bike_stands'), station.get('available_bikes'), int(station.get('banking')), station.get('bike_stands'), int(station.get('bonus')), station.get('contract_name'), station.get('last_update'), station.get('name'), station.get('number'), station.get('position').get('lat'), station.get('position').get('lng'), station.get('status')
+        )
+        connection.execute("insert into station values(%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s)", vals)
+        print(vals)
+        print("i got to this point")
+    return
+
+def main():
+
+    # run forever...
+    # while True:
+    try:
+        response = requests.get(STATIONS_URI, params={"apiKey": APIKEY, "contract": NAME})
+        print("i have made my request")
+        # print(r.url)
+        # print(r.status_code)
+        # print(json.loads(r.text))
+        # store(json.loads(r.text))
+        # now = datetime.datetime.now()
+        # print(r,now)
+        # a = requests.get("{}/{}".format(STATIONS_URI, 42),params={"apiKey": APIKEY, "contract": NAME})
+        
+        print('_____________')
+        pprint(json.loads(response.text))
+
+        # print(type(response.text))
+
+        stations_to_db(response.text)
+
+        # now sleep for 5 minutes
+        time.sleep(20)
+    except:
+        # if there is any problem, print the traceback
+        print(traceback.format_exc())
+    return
+
+if __name__ == "__main__":
+    main()
+# 
--- sleepscrape.py
+++ sleepscrape.py
@@ -6,7 +6,9 @@ from bs4 import BeautifulSoup
 import json
 from pprint import pprint
 import csv
+import time
 from time import sleep
+import traceback
 
 
 APIKEY = "24ac0c9369d539a9c2cdb7aa8bac2bd4dfaf022c"
@@ -15,17 +17,43 @@ STATIONS_URI = "https://api.jcdecaux.com/vls/v1/stations"
 # def main():
 
 
- # run forever...
-while True:
-    try:
-        r = requests.get(STATIONS_URI, params={"apiKey": APIKEY, "contract": NAME})
-        print("ok")
-        print(r.url)
-        print(r.status_code)
-        # store(json.loads(r.text))
+def main():
 
-        time.sleep(5)
-    except:
-    # if there is any problem, print the traceback
-        print("traceback")
-#  return
+    # run forever...
+    while True:
+            try:
+                r = requests.get(STATIONS_URI, params={"apiKey": APIKEY, "contract": NAME})
+                print("ok")
+                print(r.url)
+                print(r.status_code)
+                # print(json.loads(r.text))
+                # store(json.loads(r.text))
+                
+                a = requests.get("{}/{}".format(STATIONS_URI, 42),params={"apiKey": APIKEY, "contract": NAME})
+                pprint(json.loads(a.text))
+                # now sleep for 5 minutes
+                time.sleep(5)
+            except:
+                # if there is any problem, print the traceback
+                print(traceback.format_exc())
+    return
+
+
+if __name__ == "__main__":
+    main()
+
+
+#  # run forever...
+# while True:
+#     try:
+#         r = requests.get(STATIONS_URI, params={"apiKey": APIKEY, "contract": NAME})
+#         print("ok")
+#         print(r.url)
+#         print(r.status_code)
+#         # json.loads(r.text)
+
+#         # time.sleep(5)
+#     except:
+#     # if there is any problem, print the traceback
+#         print("traceback")
+# #  return
